a78a6e4d3ee95eb805cf6616efa4800d057d31a6
madereyzu04-star <madereyzu04@gmail.com>
2026-08-19 19:20:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+// =====================================
+// HALAMAN 1 → HALAMAN 2
+// =====================================
+
 function bukaWebsite() {
+
     const pembuka = document.getElementById("pembuka");
     const utama = document.getElementById("utama");
 
+    // Sembunyikan halaman pembuka
     pembuka.hidden = true;
+
+    // Tampilkan halaman utama
     utama.hidden = false;
 }
 
 
+// =====================================
+// TOMBOL LANJUT → PESAN
+// =====================================
+
 function bukaPesan() {
+
     const pesan = document.getElementById("pesan");
 
+    // Tampilkan pesan
     pesan.hidden = false;
 }
